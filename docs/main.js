45ec169359e215aeb056d8098e852ddeb31db11c
dallasplunkett <dallasplunkett@gmail.com>
2025-02-16 15:27:52 -0800

    even more refactoring: dropped manual bin calculations for histogram helper function

--- docs/main.js
+++ docs/main.js
@@ -24,32 +24,28 @@ svg.append("text")
     .text("Density");
 
 // scales and axis setup
-const minTemp = 35.0;
-const maxTemp = 40.0;
-
 const x = d3
     .scaleLinear()
-    .domain([minTemp, maxTemp])
     .range([0, width]);
-
 const xAxis = svg
     .append("g")
-    .attr("transform", `translate(0,${height})`)
-    .call(d3.axisBottom(x).tickValues(d3.range(35, 41, 1)));
+    .attr("transform", `translate(0,${height})`);
 
 const y = d3
     .scaleLinear()
     .range([height, 0]);
-
 const yAxis = svg
     .append("g");
 
 // on csv load (MAIN ENTRY POINT)
-d3.csv("../data/small_sample.csv").then(data => {
-    // correct data types
+d3.csv("../data/tidy.csv").then(data => {
     for (let i = 0; i < data.length; i++) {
         data[i].temperature = +data[i].temperature;
     }
+
+    // set initial domain
+    x.domain(d3.extent(data, d => d.temperature));
+    xAxis.call(d3.axisBottom(x).tickFormat(d3.format(".1f")));
 
     update(data, "all");
 
@@ -65,8 +61,18 @@ function update(data, gender) {
         filteredData = data.filter(d => d.gender.toLowerCase() === gender);
     }
 
+    // generate bins with histogram helper
+    const histogram = d3.histogram()
+        .value(d => d.temperature)
+        .domain(x.domain())
+        .thresholds(x.ticks(20));
+
     // compute bins
-    const bins = computeBins(filteredData);
+    const bins = histogram(filteredData);
+    const totalCount = filteredData.length;
+    for (let i = 0; i < bins.length; i++) {
+        bins[i].density = bins[i].length / (totalCount * (bins[i].x1 - bins[i].x0));
+    }
 
     // update scales and animate
     y.domain([0, d3.max(bins, d => d.density)]);
@@ -82,31 +88,4 @@ function update(data, gender) {
         .attr("width", d => x(d.x1) - x(d.x0) - 1)
         .attr("height", d => height - y(d.density))
         .attr("fill", "steelblue");
-}
-
-function computeBins(data) {
-    // generate bins
-    const binWidth = 0.5;
-    const binEdges = d3.range(minTemp, maxTemp + binWidth, binWidth);
-    const bins = [];
-    for (let i = 0; i < binEdges.length - 1; i++) {
-        bins.push({
-            x0: binEdges[i],
-            x1: binEdges[i + 1],
-            density: 0
-        });
-    }
-
-    // accumulate densities per bin
-    const totalCount = data.length || 1;
-    for (let i = 0; i < data.length; i++) {
-        for (let j = 0; j < bins.length; j++) {
-            if (data[i].temperature >= bins[j].x0 && data[i].temperature < bins[j].x1) {
-                bins[j].density += 1 / (totalCount * (bins[j].x1 - bins[j].x0));
-                break;
-            }
-        }
-    }
-
-    return bins;
 }
